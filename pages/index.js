b5016c08ad47ad4419d5608976af228a1df1b5e0
Bala45murugan <61908666+Bala45murugan@users.noreply.github.com>
2022-03-02 18:39:05 +0530

    responsive updated

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home(){
 
   const movies=[
     {movie:"Beast",img:"/images/beast.jpg"},
-    {movie:"KGF 2",img:"/images/kgf.jpg"},
+    {movie:"KGF2",img:"/images/kgf.jpg"},
     {movie:"RRR",img:"/images/rrr.jpg"},
     {movie:"Valimai",img:"/images/valimai.jpg"},
     {movie:"Don",img:"/images/don.jpg"}
@@ -42,16 +42,17 @@ export default function Home(){
 
   return(
     <>
-      <nav className='flex flex-row bg-gray-100 h-16'>
-        <h1 className='text-2xl font-bold pl-6 pt-4'>Book My Cinema</h1>
-        {
+    <div className='sm:w-max-full'>
+      <nav className='flex flex-row bg-gray-100 h-16 sm:bg-white sm:w-max-full'>
+        <h1 className='text-2xl font-bold pl-6 sm:text-3xl sm:font-bold sm:pt-8 sm:absolute'>Book My Cinema</h1>
+        {/* {
           input?
           <input type="text" placeholder='Search for movies' className=' mt-4 mb-4 border border-gray-400 px-2 rounded '></input>
           :
           (<div></div>)
-        }
-        <div className=' ml-96'>
-        <select className='mt-2 bg-gray-100 ' value={input} onChange={handleChange}>
+        } */}
+        <div className='sm:pl-72 sm:flex flex-row lg:ml-96'>
+        <select className='sm:mt-8 mt-2 sm:text-xl ' value={input} onChange={handleChange}>
         <option value="" disabled selected>Select your City</option>
           <option value="chennai">Chennai</option>
           <option value="coimbatore">Coimbatore</option>
@@ -65,7 +66,17 @@ export default function Home(){
           <option value="kanchipuram">Kanchipuram</option>
           <option value="kanyakumari">Kanyakumari</option>
         </select>
-        <button className="w-28 mx-12 mt-4 h-8 bg-white text-blue-500 font-bold hover:bg-blue-500 hover:text-white px-2 rounded-2xl">SignUp</button>
+        {
+          input ?
+          (
+            <div></div>
+          )
+          :
+          (
+            <button className="sm:mt-8 sm:text-xl w-28 ml-12 mt-4 h-8 bg-white text-blue-500 font-bold hover:bg-blue-500 hover:text-white px-2 rounded-2xl">SignUp</button>
+
+          )
+        }
         </div>
         
       </nav>
@@ -76,23 +87,23 @@ export default function Home(){
         ?
         (
           <div>
-          <h1 className='text-red-700 font-bold mt-8 text-3xl pl-6'>Theaters</h1>
+          <h1 className='text-red-700 font-bold mt-8 text-3xl pl-6 sm:pt-12 sm:text-3xl'>Theaters</h1>
           
-          <div className=' grid grid-flow-col grid-rows-1 gap-6 mt-6'>
+          <div className=' grid grid-flow-col grid-rows-1 gap-6 mt-6 sm:grid-rows-3'>
           
             {
             found.map((e,index)=>(
               <div key={index} className="">
                 
-              <div  className='pl-8 pt-4 w-52 h-44 mx-24 mt-6 rounded-xl border border-red-300 transition delay-50 ease-in-out hover:scale-110 hover:bg-blue-100 hover:shadow-2xl'>
-              <div className='flex flex-row pl-28 '>
+              <div  className='pl-8 pt-4 w-52 sm:w-96 sm:ml-32 h-44 mx-24 mt-6 rounded-xl border border-red-300 transition delay-50 ease-in-out hover:scale-110 hover:bg-blue-100 hover:shadow-2xl'>
+              <div className='flex flex-row pl-28 sm:ml-40'>
               <img src="/images/star.png" className="w-8 h-6" alt="a"></img>
               <p className="px-2 ">{e.rating}</p>
-              </div><span className='font-bold text-lg pt-2' >{e.tname}
+              </div><span className='font-bold text-lg pt-2 sm:pl-20' >{e.tname}
               </span>
-              <div className='pt-2 pl-8'>{e.show}</div>
+              <div className='pt-2 pl-8 sm:pl-32'>{e.show}</div>
               <Link href="ticket">
-                <button value={e.tname} onClick={(e)=>(setTheater(e.target.value))} className='border border-blue-400 px-2 py-1 rounded ml-8 mt-4 text-blue-400 bg-transparent hover:bg-blue-500 hover:text-white font-bold'>Book</button>
+                <button value={e.tname} onClick={(e)=>(setTheater(e.target.value))} className=' sm:ml-32 border border-blue-400 px-2 py-1 rounded ml-8 mt-4 text-blue-400 bg-transparent hover:bg-blue-500 hover:text-white font-bold'>Book</button>
               </Link>
               </div>
               </div>
@@ -108,12 +119,12 @@ export default function Home(){
         (
           <div>
           <h1 className='text-red-700 font-bold mt-24 text-3xl pl-6'>Recommended Movies</h1>
-          <div className=' grid grid-flow-col grid-rows-1 gap-6'>
+          <div className=' grid grid-flow-col grid-rows-1 gap-6 sm:grid-flow-col sm:pt-4  sm:grid-rows-3 sm:gap-24 '>
             {
               movies.map((e,index)=>(
                 <div key={index} className="transition delay-50 ease-in-out hover:scale-110">
-                <img src={e.img} alt="a" className=' w-56 h-72 mx-4 mt-6 rounded-xl'></img>
-                <h2 className='pl-20 mt-4 font-bold'>{e.movie}</h2>
+                <img src={e.img} alt="a" className=' w-56 h-72 mx-4 mt-6 rounded-xl sm:mx-20'></img>
+                <h2 className='pl-20 mt-4 font-bold sm:pl-36 sm:text-2xl'>{e.movie}</h2>
                 </div>
               ))
             }
@@ -130,6 +141,8 @@ export default function Home(){
   } */}
  
     {/* <Ticket props={input}></Ticket> */}
+
+    </div>
     </>
   )
 }
